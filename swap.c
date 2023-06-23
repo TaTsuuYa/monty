3770ecf7c199aa5d06cd75558d36ab3f6d95aaf0
Taoufik Yahyaoui <TaTsuuYa>
2023-06-23 00:06:42 +0000

    3. swap

--- swap.c
+++ swap.c
@@ -13,14 +13,14 @@ void swap(stack_t **stack, unsigned int line_number)
 	el_1 = *stack;
 	if (el_1 == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	el_2 = el_1->next;
 	if (el_2 == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	t = el_2->next;
